042a85a4adc6be58a11eaedc0d1d69a99cfaf87a
Tim Goll <tim@timgoll.de>
2019-10-06 10:32:44 +0200

    fixed wrong function call

--- lua/autorun/gimmiedatsilkicon.lua
+++ lua/autorun/gimmiedatsilkicon.lua
@@ -1,7 +1,7 @@
 --the clients need the ttt icon i used in the menu so we will send it here via the workshop
 if SERVER then
-	resource.Add("materials/icon16/addons.png")
-	resource.Add("materials/icon16/ttt.png")
+	resource.AddFile("materials/icon16/addons.png")
+	resource.AddFile("materials/icon16/ttt.png")
 
 	resource.AddWorkshop("314701418")
 end
