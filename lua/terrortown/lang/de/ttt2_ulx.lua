14866f9a05722121225618e9b94c4a8c893e6f23
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-11-26 12:46:43 +0100

    Fix wrong folder

--- lua/terrortown/lang/de/ttt2_ulx.lua
+++ lua/terrortown/lang/de/ttt2_ulx.lua
@@ -2,4 +2,4 @@ local L = LANG.GetLanguageTableReference("de")
 
 L["header_addons_ttt2_ulx"] = "Allgemeine Debug Einstellungen"
 L["help_server_addons_ttt2_ulx_ulib_debug"] = "Aktiviere diese ConVar um Stack-Traces zu ermöglichen und die Addons zu debuggen, die normalerweise ULIB crashen würden, da der Parameter default_value nicht als string in ULib.replicatedWritableCvar benutzt wurde."
-L["label_ulib_debug_enable"] = "Enable Ulib Stack Trace"
+L["label_ulib_debug_enable"] = "Aktiviere Ulib Stack-Traces"
