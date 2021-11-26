c705bb63ad79ea1ab5b1c4abe4e38cebe5daf4e3
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-11-26 12:31:45 +0100

    Add F1 Menu support and localization

---
+++ lua/terrortown/lang/de/ttt2_ulx.lua
@@ -0,0 +1,5 @@
+local L = LANG.GetLanguageTableReference("de")
+
+L["header_addons_ttt2_ulx"] = "Allgemeine Debug Einstellungen"
+L["help_server_addons_ttt2_ulx_ulib_debug"] = "Aktiviere diese ConVar um Stack-Traces zu ermöglichen und die Addons zu debuggen, die normalerweise ULIB crashen würden, da der Parameter default_value nicht als string in ULib.replicatedWritableCvar benutzt wurde."
+L["label_ulib_debug_enable"] = "Enable Ulib Stack Trace"
