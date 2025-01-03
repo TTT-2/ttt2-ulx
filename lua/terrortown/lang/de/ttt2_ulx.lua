c4e004fa49f92db37f6b065f3be275dbe408b781
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2025-01-03 22:15:39 +0100

    Remove fix (#21)

--- lua/terrortown/lang/de/ttt2_ulx.lua
+++ lua/terrortown/lang/de/ttt2_ulx.lua
@@ -1,9 +1,7 @@
 local L = LANG.GetLanguageTableReference("de")
 
 L["title_addons_ttt2_ulx"] = "TTT2-ULX"
-L["header_addons_ttt2_ulx"] = "Allgemeine Debug Einstellungen"
-L["help_server_addons_ttt2_ulx_ulib_debug"] = "Aktiviere diese ConVar um Stack-Traces zu ermöglichen und die Addons zu debuggen, die normalerweise ULIB crashen würden, da der Parameter default_value nicht als string in ULib.replicatedWritableCvar benutzt wurde."
-L["label_ulib_debug_enable"] = "Aktiviere Ulib Stack-Traces"
+L["header_addons_ttt2_ulx"] = "Allgemeine Einstellungen"
 L["menu_ulx_ported_addons_title"] = "Server Addons (ULX Port)"
 L["menu_ulx_ported_addons_description"] = "Serverweite Adminsettings für Addons die in Ulx eingetragen worden."
 L["menu_ulx_ported_addons_settings"] = "Einstellungen"
--- lua/terrortown/lang/en/ttt2_ulx.lua
+++ lua/terrortown/lang/en/ttt2_ulx.lua
@@ -1,9 +1,7 @@
 local L = LANG.GetLanguageTableReference("en")
 
 L["title_addons_ttt2_ulx"] = "TTT2-ULX"
-L["header_addons_ttt2_ulx"] = "General Debug Settings"
-L["help_server_addons_ttt2_ulx_ulib_debug"] = "Enable this ConVar to activate stack traces to debug addons, that would cause normal ULIB to crash, because no string is used as default_value in ULib.replicatedWritableCvar"
-L["label_ulib_debug_enable"] = "Enable Ulib Stack Trace"
+L["header_addons_ttt2_ulx"] = "General Settings"
 L["menu_ulx_ported_addons_title"] = "Server Addons (ULX Port)"
 L["menu_ulx_ported_addons_description"] = "Serverwide admin only settings for addons that were registered with ulx."
 L["menu_ulx_ported_addons_settings"] = "Settings"
